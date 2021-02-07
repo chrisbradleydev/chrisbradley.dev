4edce90bedd3467d04fd8358d0df67b8449afab1
Chris Bradley <chris_bradley@sweetwater.com>
2021-02-07 00:45:52 -0500

    update nav

--- components/nav.js
+++ components/nav.js
@@ -1,19 +1,21 @@
 import Link from 'next/link';
 import styles from './nav.module.scss';
 
-const Nav = () => (
-    <nav className={styles.nav}>
-        <section className={styles.nav__section}>
-            <ul className={styles.nav__right}></ul>
-            <ul className={styles.nav__left}>
-                <li>
-                    <Link href="/">
-                        <a>CBD</a>
-                    </Link>
-                </li>
-            </ul>
-        </section>
-    </nav>
-);
+function Nav() {
+    return (
+        <nav className={styles.nav}>
+            <section className={styles.nav__section}>
+                <ul className={styles.nav__right}></ul>
+                <ul className={styles.nav__left}>
+                    <li>
+                        <Link href="/">
+                            <a>CBD</a>
+                        </Link>
+                    </li>
+                </ul>
+            </section>
+        </nav>
+    );
+}
 
 export default Nav;
