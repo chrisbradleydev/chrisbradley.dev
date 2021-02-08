4fcec2a112fa4939416996a4ef60fe4785481ece
Chris Bradley <chris_bradley@sweetwater.com>
2021-02-07 22:21:27 -0500

    update layout

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import PropTypes from 'prop-types';
 import styles from './layout.module.scss';
 import Nav from './nav';
@@ -8,11 +7,11 @@ export const fullName = 'Chris Bradley';
 function Layout({ children }) {
     return (
         <div className={styles.container}>
-            <Head />
-            <Nav />
-            <header />
-            <main>{children}</main>
-            <footer />
+            <header className={styles.container__header}>
+                <Nav />
+            </header>
+            <main className={styles.container__main}>{children}</main>
+            <footer className={styles.container__footer} />
         </div>
     );
 }
