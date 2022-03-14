15f6c746090da0ccf550b8f1cab3dbf8b89df0b2
Christopher Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2022-03-14 00:24:01 -0400

    copy: update homepage title

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import Head from 'next/head';
-import Layout, { fullName, projectName } from '../components/layout';
+import Layout, { projectName } from '../components/layout';
 
 function Home() {
     return (
         <Layout home>
             <Head>
-                <title>
-                    {fullName}&apos;s personal website. | {projectName}
-                </title>
+                <title>{projectName}</title>
             </Head>
             <section className="h-screen">
                 <div />
