2873a654ff423f6c6b4be41ddd29906e224b4f45
Chris Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2022-06-05 22:08:30 -0400

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,1 @@
-const path = require('path');
-
-module.exports = {
-    sassOptions: {
-        includePaths: ['node_modules', 'styles'].map(d => path.join(__dirname, d)),
-    },
-};
+module.exports = {};
