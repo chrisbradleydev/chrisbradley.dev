730532853f5aecc627ed0be49dcff0f70704e75d
Chris Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2024-03-17 00:59:21 -0400

    create hero component

---
+++ components/hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <header className="flex items-center justify-center h-min">
+      <div className="text-center py-24">
+        <h1 className="text-6xl font-bold dark:text-white uppercase">
+          From my <span className="text-pink-400">brain</span> to yours.
+        </h1>
+        <p className="text-gray-600 mt-4 text-2xl">
+          Started from GeoCities, now we&apos;re here.
+        </p>
+      </div>
+    </header>
+  )
+}
+
+export default Hero
