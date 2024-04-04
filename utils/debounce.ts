f9e42350d2772b10e993e1c3765d7280d80341cf
Chris Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2024-04-03 22:35:15 -0400

    update debounce

--- utils/debounce.ts
+++ utils/debounce.ts
@@ -1,7 +1,9 @@
-const debounce = (fn: Function, ms = 100) => {
-  let timeoutId: ReturnType<typeof setTimeout>
-  return function (this: any, ...args: any[]) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const debounce = <T extends (...args: any[]) => any>(fn: T, ms = 100) => {
+  let timeoutId: ReturnType<typeof setTimeout> | undefined
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
     clearTimeout(timeoutId)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     timeoutId = setTimeout(() => fn.apply(this, args), ms)
   }
 }
