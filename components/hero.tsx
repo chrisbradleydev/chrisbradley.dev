9e3ce3e75408b72b0a2471ba94c7c20a55f0b17d
Chris Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2024-11-17 10:45:37 -0500

    update hero text

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,7 +3,7 @@ const Hero = () => {
     <div className="flex w-full items-center justify-center">
       <div className="flex flex-col items-center py-10 text-center">
         <h1 className="text-4xl font-bold uppercase dark:text-white md:text-5xl lg:text-6xl">
-          From my <span className="text-pink-400">brain</span> to yours.
+          From my <span className="text-pink-400">brain</span> to yours
         </h1>
         <p className="mt-4 text-2xl text-gray-600 md:text-3xl lg:text-4xl">
           Started from GeoCities, now we&apos;re here.
