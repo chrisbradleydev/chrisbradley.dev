b96918b296195f16a83d0230fbf7490ff2c958b1
Chris Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2025-02-15 20:47:56 -0500

    add QuoteHeader and QuoteBody

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -18,6 +18,38 @@ const MDXHeader = ({children}: {children: string}) => {
   )
 }
 
+const QuoteHeader = ({children}: {children: string}) => {
+  return (
+    <div
+      className="my-10
+      text-center
+      text-3xl
+      font-extrabold
+      tracking-tight
+      text-gray-900
+      dark:text-gray-100
+      sm:text-4xl
+      md:text-5xl"
+    >
+      <h1>
+        <span className="decoration-3 underline decoration-pink-300 decoration-dashed underline-offset-4">
+          {children}
+        </span>
+      </h1>
+    </div>
+  )
+}
+
+const QuoteBody = ({children}: {children: string}) => {
+  return (
+    <div className="indent-8 text-xl">
+      <span className="text-pink-300">&quot;</span>
+      {children}
+      <span className="text-pink-300">&quot;</span>
+    </div>
+  )
+}
+
 export const MDXLayoutRenderer = ({
   mdxSource,
   ...rest
@@ -25,5 +57,7 @@ export const MDXLayoutRenderer = ({
   mdxSource: string
 }) => {
   const MDXLayout = React.useMemo(() => getMDXComponent(mdxSource), [mdxSource])
-  return <MDXLayout components={{MDXHeader}} {...rest} />
+  return (
+    <MDXLayout components={{MDXHeader, QuoteHeader, QuoteBody}} {...rest} />
+  )
 }
