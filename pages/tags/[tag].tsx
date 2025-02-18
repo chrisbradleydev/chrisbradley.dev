c8f346f69786b8124da2685727033e140b8f39a2
Chris Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2025-02-17 23:05:55 -0500

    update tag links

--- pages/tags/[tag].tsx
+++ pages/tags/[tag].tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Layout from '~/components/layout'
 import {
   FrontmatterPost,
@@ -6,6 +7,23 @@ import {
   getAllQuotes,
 } from '~/utils/mdx'
 import {getAllTags, TagsCount} from '~/utils/tags'
+
+const romanNumerals: [string[], string[]] = [
+  ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+  ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
+]
+
+function intToRoman(num: number): string {
+  const tensDigits = romanNumerals[1]
+  const onesDigits = romanNumerals[0]
+  if (!tensDigits || !onesDigits) return ''
+  const tens = Math.floor(num / 10)
+  const ones = num % 10
+  return (
+    (tens < tensDigits.length ? tensDigits[tens]! : '') +
+    (ones < onesDigits.length ? onesDigits[ones]! : '')
+  )
+}
 
 function Tag({
   posts,
@@ -20,10 +38,31 @@ function Tag({
     <Layout pageName={tag}>
       <ul>
         {posts.map(post => (
-          <li key={post.slug}>{post.title ?? 'Untitled'}</li>
+          <li key={post.slug}>
+            <Link
+              className="hover:text-pink-400 dark:hover:text-pink-300"
+              href={`/blog/${post.slug}`}
+            >
+              {post.title ?? 'Untitled'}
+            </Link>
+          </li>
         ))}
         {quotes.map(quote => (
-          <li key={quote.slug}>{quote.author}</li>
+          <li key={quote.slug}>
+            <Link
+              className="hover:text-pink-400 dark:hover:text-pink-300"
+              href={`/quotes/${quote.slug}`}
+            >
+              {quote.author}
+              {(() => {
+                const match = /-(\d+)$/.exec(quote.slug)
+                const num = match?.[1]
+                return num && parseInt(num) > 1
+                  ? ` ${intToRoman(parseInt(num))}`
+                  : ''
+              })()}
+            </Link>
+          </li>
         ))}
       </ul>
     </Layout>
